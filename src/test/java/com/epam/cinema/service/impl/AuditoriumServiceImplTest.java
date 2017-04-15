d0350ed33453cb1eccd98a474513e50294e4ad7c
kampone <vladislavkaminski2806@gmail.com>
2017-04-15 22:34:52 +0300

    update maven build with cobertura

--- src/test/java/com/epam/cinema/service/impl/AuditoriumServiceImplTest.java
+++ src/test/java/com/epam/cinema/service/impl/AuditoriumServiceImplTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.*;
 
 
 public class AuditoriumServiceImplTest {
+
     @Test
     public void getAll() throws Exception {
     }
--- src/test/java/com/epam/cinema/service/impl/BookingServiceImplTest.java
+++ src/test/java/com/epam/cinema/service/impl/BookingServiceImplTest.java
@@ -1,23 +1,63 @@
 package com.epam.cinema.service.impl;
 
+import com.epam.cinema.model.*;
+import com.epam.cinema.service.DiscountService;
+import com.epam.cinema.service.TicketService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Arrays;
 
-/**
- * Created by vladi on 06.04.2017.
- */
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+
+@RunWith(MockitoJUnitRunner.class)
 public class BookingServiceImplTest {
+    @Mock
+    private DiscountService discountService;
+
+    @Mock
+    private TicketService ticketService;
+
+    @InjectMocks
+    private BookingServiceImpl bookingService;
+
     @Test
     public void getTicketsPrice() throws Exception {
+        Mockito.when(discountService.getDiscount(any(User.class), any(Event.class), any(LocalDateTime.class), anyInt())).then(invocationOnMock -> 0);
+
+        BigDecimal price = bookingService.getTicketsPrice(new Event(1L, "event", new BigDecimal(10), Rating.LOW),
+                LocalDateTime.now(),
+                new User(),
+                Arrays.asList(new Seat(1L,1, 1, false))
+        );
+
+        assertEquals(new BigDecimal(10), price);
     }
 
     @Test
     public void bookTicket() throws Exception {
+        Ticket ticket = new Ticket(null, null, null, null, null, false);
+
+        bookingService.bookTicket(ticket);
+
+        assertTrue(ticket.isBooked());
+
     }
 
     @Test
     public void getPurchasedTicketsForEvent() throws Exception {
+        bookingService.getPurchasedTicketsForEvent(null, null);
+
+        Mockito.verify(ticketService).getPurchasedTicketsForEventAndDate(null, null);
     }
 
 }
